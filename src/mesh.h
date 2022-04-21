154318d4bc5172a783401584c30bb081792e8423
JsM96 <juansebastian.marquerie01@estudiant.upf.edu>
2022-04-21 12:31:43 +0200

    Divides the issue and started work on it

--- src/mesh.h
+++ src/mesh.h
@@ -35,6 +35,8 @@ struct sMesh {
     uint16_t    face_count      = 0;
     uint16_t    indexing_count  = 0;
 
+    uint16_t    *face_edges = NULL; // TODO store the vertices of each edge
+
     uint16_t    *vertices_index = NULL;
 
     sRawVertex  *vertices     = NULL;
